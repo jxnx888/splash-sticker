6fd8e559ae3fc2c74f927930c9e059494d5f2f7d
Xin Ning <ningxin1007@hotmail.com>
2025-08-12 14:19:45 -0400

    fix: mouse helper

--- src/components/Scene/ThreeScene.tsx
+++ src/components/Scene/ThreeScene.tsx
@@ -289,7 +289,7 @@ export default function ThreeScene({previewDecalsRef}: ThreeSceneServerProps) {
     const height = mountRef.current!.clientHeight;
     const leftDecalWidth = previewDecalsRef?.current?.clientWidth || 0;
 
-    mouseRef.current.x = (x / (width + leftDecalWidth)) * 2 - 1 - 0.3;
+    mouseRef.current.x = (x / (width + leftDecalWidth)) * 2 - 1;
     mouseRef.current.y = -(y / height) * 2 + 1 + 0.12;
 
     raycaster.setFromCamera(mouseRef.current, camera);
